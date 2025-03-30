809f0b7803f2e89cca6ed1732928d65b756633fb
filip-byte <ohboytoby@gmail.com>
2025-03-30 09:10:33 +0100

    feat: add Artwork entity for PostgreSQL persistence

---
+++ src/main/java/com/exhibitionapp/model/entity/Artwork.java
@@ -0,0 +1,47 @@
+package com.exhibitionapp.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Artwork {
+
+    @Id
+    private int id;
+
+    private String title;
+
+    private String imageId;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy
+                ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass()
+                : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy
+                ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass()
+                : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Artwork artwork = (Artwork) o;
+        return Objects.equals(this.id, artwork.id);
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy
+                ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode()
+                : getClass().hashCode();
+    }
+}
