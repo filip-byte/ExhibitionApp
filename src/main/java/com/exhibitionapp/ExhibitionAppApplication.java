7fd7c65855fd64af217ea26d4b7a643ccdd57062
filip-byte <ohboytoby@gmail.com>
2025-03-30 09:10:03 +0100

    feat: add Spring Boot application entry point

---
+++ src/main/java/com/exhibitionapp/ExhibitionAppApplication.java
@@ -0,0 +1,12 @@
+package com.exhibitionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExhibitionAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExhibitionAppApplication.class, args);
+    }
+}
