83b5c2aa47b978bac3f5ca5cb5c67da7ee960bc4
filip-byte <ohboytoby@gmail.com>
2025-03-30 18:33:11 +0100

    feat: add findByName method to GalleryRepository for name uniqueness check

--- src/main/java/com/exhibitionapp/repository/GalleryRepository.java
+++ src/main/java/com/exhibitionapp/repository/GalleryRepository.java
@@ -4,6 +4,9 @@ import com.exhibitionapp.model.entity.Gallery;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface GalleryRepository extends JpaRepository<Gallery, Long> {
+    Optional<Gallery> findByName(String name);
 }
