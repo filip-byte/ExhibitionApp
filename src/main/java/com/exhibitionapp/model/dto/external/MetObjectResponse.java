bfb4f979508652eed9098091250ba414055800b9
filip-byte <ohboytoby@gmail.com>
2025-03-30 12:17:42 +0100

    feat: add MetObjectResponse DTO for MET API object details

---
+++ src/main/java/com/exhibitionapp/model/dto/external/MetObjectResponse.java
@@ -0,0 +1,10 @@
+package com.exhibitionapp.model.dto.external;
+
+import lombok.Data;
+
+@Data
+public class MetObjectResponse {
+    private int objectID;
+    private String title;
+    private String primaryImageSmall;
+}
