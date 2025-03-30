a42a32ebebc35203178e51bea87b1eef26154aee
filip-byte <ohboytoby@gmail.com>
2025-03-30 09:11:48 +0100

    feat: add ArticArtwork DTO for individual Artic artwork data

---
+++ src/main/java/com/exhibitionapp/model/dto/external/ArticArtwork.java
@@ -0,0 +1,15 @@
+package com.exhibitionapp.model.dto.external;
+
+import lombok.Data;
+
+@Data
+public class ArticArtwork {
+
+    private float _score;
+
+    private int id;
+
+    private String title;
+
+    private String image_id;
+}
